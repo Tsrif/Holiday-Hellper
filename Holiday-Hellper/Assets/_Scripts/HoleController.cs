4f27e15182308185f834d7878d08f1fb525d9650
Unknown <rivera_richard1@columbusstate.edu>
2018-03-21 16:45:15 -0400

    Level1 Scene
    Integrated the level 1 scene into the project

--- Holiday-Hellper/Assets/_Scripts/HoleController.cs
+++ Holiday-Hellper/Assets/_Scripts/HoleController.cs
@@ -7,16 +7,6 @@ public class HoleController : MonoBehaviour {
 
     public static event Action winCondition;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Kid")
