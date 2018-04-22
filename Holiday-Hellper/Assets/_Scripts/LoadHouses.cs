34d553f13434484158ec555f989a6df58efbd1b4
Unknown <hicks_james1@columbusstate.edu>
2018-04-22 03:33:05 -0400

    delayed start not working, heres code
    The delayed start isn't working but I have included this code just in case I am able to get it working tommorow

---
+++ Holiday-Hellper/Assets/_Scripts/LoadHouses.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadHouses : MonoBehaviour {
+
+    private int timer;
+    public GameObject[] houses;
+
+    private void Start()
+    {
+        //timer = 20;
+        //StartCoroutine("loading");
+    }
+
+    IEnumerator loading() {
+        for (int i = 0; i < houses.Length; i++) {
+            //yield return new WaitForSeconds(timer);
+            yield return new WaitForSeconds(10);
+            Instantiate(houses[i]);
+            //timer -= 5;
+        }
+    }
+    private void OnLevelWasLoaded(int level)
+    {
+        StartCoroutine("loading");
+    }
+}
